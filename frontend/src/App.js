a543df6a5bbbceb2faa66b9276c6d08308d7232e
Mario Schwabe Filho <marioschwabefilho@gmail.com>
2022-03-01 18:25:04 +0100

    initial frontend basics

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+
+  const [students, setStudents] = useState([])
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
